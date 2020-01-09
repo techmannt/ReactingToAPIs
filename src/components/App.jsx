3989ac44c8e65b2c0bdb6f752d68f224cf006ae8
ntompkins <ntompkins@classworks.com>
2020-01-09 10:58:42 -0600

    Completed Phase Two.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import 'isomorphic-fetch';
 import "es6-promise";
 import DisplayCard from './DisplayCard';
+import Logo from "../logo.png";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      filmsInfo: []
+      filmsInfo: [],
+      loaded: false
     }
   }
 
@@ -18,24 +20,37 @@ class App extends Component {
 
   }
 
+  handleButtonClick() {  // The button has been click so set the "loaded" property in this.state.
+    this.setState({ loaded: true });
+  }
+
   render() {
+    if (this.state.loaded) {  // If the button to load the films has been clicked, then show them.
+      return (
+        <>
+          <main className="container">
+            {this.state.filmsInfo.map(film => {
+              return (
 
-    return (
-      <>
-        <main className="container">
-          {this.state.filmsInfo.map(film => {
-            return (
+                <div className="py-1" key={film.id}>
+                  <DisplayCard films={film} />
+                </div>
 
-              <div className="py-1" key={film.id}>
-                <DisplayCard films={film} />
-              </div>
-
-            )
-          })}
-        </main>
-      </>
-    )
-
+              )
+            })}
+          </main>
+        </>
+      )
+    } else {
+      return (
+        <>
+          <p><img src={Logo} alt="logo" /></p>
+          <main className="container">
+            <p><button id="submit" className="btn btn-primary" onClick={(event) => this.handleButtonClick(event)}>Load Films!</button></p>
+          </main>
+        </>
+      )
+    }
   }
 
 }
